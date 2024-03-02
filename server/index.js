b15d5ab3cd0997914db796d8b1d5a098daa4880a
Anthony Bernard <25408995+apb305@users.noreply.github.com>
2024-03-02 17:55:07 -0500

    minor update

--- server/index.js
+++ server/index.js
@@ -23,10 +23,7 @@ app.use("/api/chats", chats);
 
 // Serve Static assets for production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client/build"));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  app.use(express.static("client/build"));
 }
 
 const port = process.env.PORT || 4000;
