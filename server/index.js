45d26b9cf19a847af1d358715a4e808c89b35d69
Anthony Bernard <25408995+apb305@users.noreply.github.com>
2024-03-03 17:01:10 -0500

    minor change

--- server/index.js
+++ server/index.js
@@ -25,9 +25,7 @@ app.use("/api/chats", chats);
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve(); 
   app.use(express.static(path.join(__dirname, "/client/dist")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-  });
+
 }
 
 const port = process.env.PORT || 4000;
