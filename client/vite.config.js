b8963cf20fe23ff82807306e72075ea27abb553b
Anthony Bernard <25408995+apb305@users.noreply.github.com>
2024-03-03 16:44:53 -0500

    minor update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://two024-openai-project-backend.onrender.com',
+      ChangeOrigin: true,
+      Rewrite: (path) => path.replace(/^\/api/, ''),
+    },
+  },
 })
